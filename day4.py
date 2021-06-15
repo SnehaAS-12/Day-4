e5b11f059e4af5718f29bdc0d03f71204063266c
SnehaAS-12 <85794682+SnehaAS-12@users.noreply.github.com>
2021-06-15 09:48:35 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+a=b=c=50
+d=a/10
+print("Divide a by 10 =",d)
+e=b*50
+print("Multiply b by 50 =",e)
+f=c+60
+print("Add c value by 60 =",f)
+
+
+string="SNEHA"
+print("Replaced string =",string.replace("E","G"))
+
+
+a=10
+b=12.5
+print("After converting a from int to float =",float(a))
+print("After converting b fron float to int =",int(b))
